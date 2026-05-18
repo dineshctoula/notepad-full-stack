04419464f65ef2d793a0d9a998a386ea8cd3d96b
dineshctoula <ds9806026041@gmail.com>
2026-05-18 12:07:28 +0545

    dto added

--- backend/api/src/auth/auth.controller.ts
+++ backend/api/src/auth/auth.controller.ts
@@ -1,17 +1,27 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
+
 import { AuthService } from './auth.service';
+
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+  ) {}
 
   @Post('register')
-  register(@Body() body: any) {
-    return this.authService.register(body.email, body.password);
+  register(@Body() dto: RegisterDto) {
+    return this.authService.register(dto);
   }
 
   @Post('login')
-  login(@Body() body: any) {
-    return this.authService.login(body.email, body.password);
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
   }
 }
